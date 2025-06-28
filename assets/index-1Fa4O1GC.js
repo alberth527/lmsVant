d892e96cb82ff3793f64ba1b728bcb4f161fbf55
alberth527 <alberth527@gmail.com>
2025-06-28 11:10:11 +0800

    Deploy to GitHub Pages

---
+++ assets/index-1Fa4O1GC.js
@@ -0,0 +1,1 @@
+import{y as e}from"./index-B6FFb5jA.js";let r=0;function u(){const n=e(),{name:t="unknown"}=n?.type||{};return`${t}-${++r}`}export{u};
--- assets/index-B0rN40JS.js
+++
@@ -1,1 +0,0 @@
-import{y as e}from"./index-NED_JcNa.js";let r=0;function u(){const n=e(),{name:t="unknown"}=n?.type||{};return`${t}-${++r}`}export{u};
